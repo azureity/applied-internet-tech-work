1896a54a78037951a54beba2a388fbdfa2050f8f
Vince <vz309@nyu.edu>
2014-09-09 10:51:00 -0400

    generateCards function added and tested

--- blackjackapp.js
+++ blackjackapp.js
@@ -1,3 +1,23 @@
 var prompt = require('sync-prompt').prompt;
 
+var generateCards = function(){
+	var suits = ['♠','♥','♦','♣'];
+	var faces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+	
+	var cards = [];
+	for(var i = 0; i < faces.length; i++){
+		for(var j = 0; j < suits.length; j++){
+			var card = {
+				face:faces[i],
+				suit:suits[j]
+			};
+			cards.push(card);
+		}
+	}
+	return cards;
+};
 
+console.log("asdf");
+var cards = generateCards();
+console.log(cards); 
+console.log(cards.length);
