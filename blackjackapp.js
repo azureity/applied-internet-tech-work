a249ddfa93937c9693c35fe906cba3c25c9a914f
Vince <vz309@nyu.edu>
2014-09-09 18:42:34 -0400

    cleaned code. added proper comments. finished and closing

--- blackjackapp.js
+++ blackjackapp.js
@@ -1,5 +1,17 @@
+/*----------------------------------
+Vincent Zhen
+Prof. Joe Versoza
+CSCI-UA.0480-002 Fall 2014
+Homework #1
+
+Simple blackjack code in javascript.
+----------------------------------*/
+
+/* Import function 'prompt' from the 'sync-prompt' module for synchronous user input */
 var prompt = require('sync-prompt').prompt;
 
+/* Generates a (sorted) deck of cards. Each card is an object.
+Returns and array of card objects. */
 var generateCards = function(){
 	var suits = ['♠','♥','♦','♣'];
 	var faces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
@@ -17,7 +29,7 @@ var generateCards = function(){
 	return cards;
 };
 
-//paramter must be an array
+/* Shuffles the deck of cards 'x' by iterating through the deck and switching it with a random card in the deck. */
 var shuffle = function(x){
 	for(var i = 0; i < x.length; i++){	
 		randomIndex = Math.floor(Math.random() * x.length);
@@ -28,10 +40,10 @@ var shuffle = function(x){
 	return x;
 };
 
-//parameter must be an array
+/* Returns the total of the hand. Ignores suits.
+Input is an array 'x' */
 var calculateHand = function(x){
 	var total = 0;
-
 	for(var i = 0; i < x.length; i++){
 		switch(x[i]){
 			case 'J':
@@ -40,6 +52,7 @@ var calculateHand = function(x){
 				total += 10;
 				break;
 			case 'A':
+				// Aces may count as 11 or 1 depending on situation
 				total += 11;
 				if(total > 21){
 					total -= 10;}
@@ -51,17 +64,18 @@ var calculateHand = function(x){
 	return total;
 };
 
-//get a simple array from hand
+/* Make an array of just the faces, stripping the suits to make it easier to implement other functions.
+Input is an array 'x' */
 var simpHand = function(x){
 	var ans = [];
 	for(var i = 0; i < x.length; i++){
 		ans.push(x[i].face);
 	}
-	//console.log("SIMPLE: " + ans);
 	return ans;
 };
 
-//x is player hand. y is AI hand
+/* Determines winner based on closeness to 21 excluding negatives which is a bust.
+The player hand is 'x' and the computer hand is 'y' for input. */
 var determineWinner = function(x, y){
 	var player = 21 - calculateHand(x);
 	var computer = 21 - calculateHand(y);
@@ -73,7 +87,7 @@ var determineWinner = function(x, y){
 	return win;	
 };
 
-//x is an array
+/* Function to print the hand 'x' */
 var printHand = function(x){
 	var hand = "";
 	for(var i = 0; i < x.length; i++){
@@ -82,85 +96,61 @@ var printHand = function(x){
 	return hand;
 };
 
-//testing space
-/*
-console.log("asdf");
-var cards = generateCards();
-console.log(cards); 
-console.log(cards.length);
-
-var shuffled = shuffle(cards);
-console.log(shuffled);
-console.log(shuffled.length);
-
-var hand = ['K', '3', '5'];
-var hand2 = ['A', 'A', 'A', '8'];
-var hand3 = ['6', 'A'];
-var hand4 = ['K', 'J', '3']
-var hand5 = ['J', 'Q', 'Q']
-
-console.log(calculateHand(hand));
-console.log(calculateHand(hand2));
-console.log(calculateHand(hand3));
-
-var winner = determineWinner(hand, hand2);
-console.log(winner);
-winner = determineWinner(hand2, hand4);
-console.log(winner);
-console.log(determineWinner(hand4, hand5));
-console.log(determineWinner(hand3, hand));
-console.log(determineWinner(hand2, hand2));
-*/
-
-//game on
+/*---------------------------
+The main block of code starts here
+---------------------------*/
+//Initialize deck of cards
 var cards = generateCards();
 cards = shuffle(cards);
 
+//Loop over games until less than 26 cards left in deck
 var gameon = true;
 while(gameon){
-var playerHand = [];
-playerHand.push(cards.pop());
-playerHand.push(cards.pop());
-//console.log(playerHand);
-var compHand = [];
-compHand.push(cards.pop());
-compHand.push(cards.pop());
-//console.log(compHand);
-
-//console.log("asdf")
-console.log("Your hand is: " + printHand(playerHand) + "... for a total of " + calculateHand(simpHand(playerHand)));
-
-var cont = true;
-while(cont){
-	var action = prompt('(h)it or (s)tay?\n> ');
-	//console.log('action::: ' + action);
-	switch(action){
-		case('h'):
-		case('hit'):
-			playerHand.push(cards.pop());
-			if(calculateHand(simpHand(playerHand)) <= 21){
-				console.log("Your hand: " + printHand(playerHand) + " (" + calculateHand(simpHand(playerHand)) + ")");
-			}
-			else cont = false;
-			break;
-		case('s'):
-		case('stay'):
-			while(calculateHand(simpHand(compHand)) < 17){
-				compHand.push(cards.pop());
-			}
-			cont = false;
-			break;
-		default:
-			console.log("Please choose either to hit or stay\n");
-			break;
-	}
+	//Deal out first hands
+	var playerHand = [];
+	playerHand.push(cards.pop());
+	playerHand.push(cards.pop());
+	var compHand = [];
+	compHand.push(cards.pop());
+	compHand.push(cards.pop());
+	console.log("Your hand is: " + printHand(playerHand) + "... for a total of " + calculateHand(simpHand(playerHand)));
 	
-}
+	//Continue with the current game
+	var cont = true;
+	while(cont){
+		var action = prompt('(h)it or (s)tay?\n> ');
+		switch(action){
+			case('h'):
+			case('hit'):
+				playerHand.push(cards.pop());
+				//Only allow continue if player did not bust
+				if(calculateHand(simpHand(playerHand)) <= 21){
+					console.log("Your hand: " + printHand(playerHand) + " (" + calculateHand(simpHand(playerHand)) + ")");
+				}
+				else cont = false;
+				break;
+			case('s'):
+			case('stay'):
+				//Deal for computer until >17
+				while(calculateHand(simpHand(compHand)) < 17){
+					compHand.push(cards.pop());
+				}
+				cont = false;
+				break;
+				//Bad input
+			default:
+				console.log("Please choose either to hit or stay\n");
+				break;
+		}
+	}
 
-console.log("Your hand: " + printHand(playerHand) + " (" + calculateHand(simpHand(playerHand)) + "), Computer hand: " + printHand(compHand) + " (" + calculateHand(simpHand(compHand)) + ")");
-console.log(determineWinner(simpHand(playerHand), simpHand(compHand)));
-console.log("There are " + cards.length + " cards left in the deck\n------------------------------------------\n");
-if(cards.length < 26) gameon = false;
+	//Tally final scores and determine a winner
+	console.log("Your hand: " + printHand(playerHand) + " (" + calculateHand(simpHand(playerHand)) + "), Computer hand: " + printHand(compHand) + " (" + calculateHand(simpHand(compHand)) + ")");
+	console.log(determineWinner(simpHand(playerHand), simpHand(compHand)));
+	console.log("There are " + cards.length + " cards left in the deck\n------------------------------------------\n");
+
+	//End game if number of cards are below 26
+	if(cards.length < 26) gameon = false;
 }
 
 console.log("Less than 26 cards left. Game over!");
